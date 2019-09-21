88293092a7fe6c14e5f9b40d5148a29f822eed9a
Mayank Khajanchi <mayankkhajnchi@gmail.com>
2019-09-21 22:19:58 +0530

    initial

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <div class="container pt-5">
+      <HelloWorld :dataset="dataset"/>
+      <HelloWorld :dataset="dataset"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import Vue from "vue";
+export default {
+  name: "app",
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      dataset: ["A", "B", "C", "D"]
+    };
+  }
+};
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <div class="row">
+      <div class="col-3 mb-2" v-for="(item, index) in dataset" :key="index">
+        {{index + 1}}<input class="ml-2" type="text" v-model="dataset[index]"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {};
+  },
+  props: ["dataset"]
+};
+</script>
